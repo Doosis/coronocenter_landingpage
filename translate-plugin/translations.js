c46c7efc46982b316e59ee7c83b2fec4c300cd95
Paul Lapok <p.lapok@doosis.com>
2022-02-27 19:56:34 +0100

    included language change dropdown

--- translate-plugin/translations.js
+++ translate-plugin/translations.js
@@ -2,36 +2,71 @@
 var lang= window.navigator.language, // Check the Browser language
 translate; // Container of all translations
 
-// Call translations json file and populate translate variable  
-$.getJSON("translate-plugin/translations.json", function(texts) {
-    translate=texts;
+// Update language dropdown
+if (lang === 'en') {
+    let element = document.getElementById('language');
+    element.value = 'English';
+}
+else if(lang === 'de'){
+    let element = document.getElementById('language');
+    element.value = 'Deutsch';
+}
+else {
+    let element = document.getElementById('language');
+    element.value = 'English';
+} 
 
-    // Translations Function: Get all the element with data-text
-    $("[data-translate]").each(function() {
-        let text= $(this).attr('data-translate'), // Save the Text into the variable
-            numbers= text.match(/\d+/g),
-            element=  $('[data-translate="'+text+'"]'),
-            postHTML;
 
-        if (numbers != null && numbers>1) 
-                text= text.replace(numbers, '%n');
-        
-        if (translate[text]!==undefined) { // Check if exist the text in translation.json                      
+startTranslation();
 
-            if (translate[text][lang]!==undefined) { // Check if exist the text in the browser language
-                postHTML= translate[text][lang];
-            } else { // If not exist the lang, show the text in primary Language (Recomend: English)
-                postHTML= text;
-            }
+function startTranslation(){
+    // Call translations json file and populate translate variable  
+    $.getJSON("translate-plugin/translations.json", function(texts) {
+
+        translate=texts;
+
+        // Translations Function: Get all the element with data-text
+        $("[data-translate]").each(function() {
+            let text= $(this).attr('data-translate'), // Save the Text into the variable
+                numbers= text.match(/\d+/g),
+                element=  $('[data-translate="'+text+'"]'),
+                postHTML;
 
             if (numbers != null && numbers>1) 
-                postHTML= postHTML.replace('%n', numbers);
+                    text= text.replace(numbers, '%n');
+            
+            if (translate[text]!==undefined) { // Check if exist the text in translation.json                      
 
-            element.html(postHTML);
+                if (translate[text][lang]!==undefined) { // Check if exist the text in the browser language
+                    postHTML= translate[text][lang];
+                } else { // If not exist the lang, show the text in primary Language (Recomend: English)
+                    postHTML= text;
+                }
 
-        } else {
-            $('[data-translate="'+text+'"]').html("ERR").css({'color':'red','font-weight':'bold'});
-            //$('[data-translate="'+text+'"]').html(text).css({'color':'red','font-weight':'bold'});
-        }
+                if (numbers != null && numbers>1) 
+                    postHTML= postHTML.replace('%n', numbers);
+
+                element.html(postHTML);
+
+            } else {
+                //$('[data-translate="'+text+'"]').html("ERR").css({'color':'red','font-weight':'bold'});
+                $('[data-translate="'+text+'"]').html(text).css({'color':'red','font-weight':'bold'});
+            }
+        });
     });
-});
+}
+
+function toggleLanguage(language) {
+    if (language === "English") {
+        lang = 'en';
+    }
+    else if(language === "Deutsch"){
+        lang = 'de';
+    }
+    else {
+        lang = 'en';
+    } 
+    
+    startTranslation();
+}
+
