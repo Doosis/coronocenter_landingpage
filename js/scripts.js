81a9e2865748befa4441eed911c62cee2867da9e
Paul Lapok <p.lapok@doosis.com>
2022-03-23 14:06:22 +0100

    hide modal when submitted

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,11 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-business-frontpage/blob/master/LICENSE)
 */
-// This file is intentionally blank
-// Use this file to add JavaScript to your project
+
+$('#button').submit(function(e) {
+    e.preventDefault();
+    // Coding
+    $('#exampleModal').modal('toggle'); //or  $('#IDModal').modal('hide');
+    return false;
+});
 
